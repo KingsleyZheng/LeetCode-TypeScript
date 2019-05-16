d3fe5598414ae7c4e7961098342fb600ef445945
KingsleyZheng <46808454+KingsleyZheng@users.noreply.github.com>
2019-05-16 14:36:06 +0800

    190516

--- LeetCode-easy.js
+++ LeetCode-easy.js
@@ -51,3 +51,26 @@ var searchInsert = function(nums, target) {
     return l;        
 };
 //执行用时 : 68 ms, 在Search Insert Position的JavaScript提交中击败了99.14% 的用户
+
+//38. 报数
+//报数序列是一个整数序列，按照其中的整数的顺序进行报数，得到下一个数。其前五项如下：
+/**
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
+1 被读作  "one 1"  ("一个一") , 即 11。
+11 被读作 "two 1s" ("两个一"）, 即 21。
+21 被读作 "one 2",  "one 1" （"一个二" ,  "一个一") , 即 1211。
+给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。
+注意：整数顺序将表示为一个字符串。
+ */
+//
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+    
+};
