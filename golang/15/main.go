f818671fbccd084dca4bedfe5256ccb07130fd24
郑成智 <46808454+KingsleyZheng@users.noreply.github.com>
2020-07-11 21:54:08 +0800

    golang 15 16

---
+++ golang/15/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+	fmt.Println(threeSum2(nums))
+}
+
+// 中等
+// 15. 三数之和
+// 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+// 注意：答案中不可以包含重复的三元组。
+
+// 好难，基本想不到还能有先排序这种操作
+// 官方题解一：排序+双指针法
+// 难点是不重复，可以先排序来做
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	for first := 0; first < n; first++ {
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		third := n - 1
+		target := -1 * nums[first]
+		for second := first + 1; second < n; second++ {
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+
+	return ans
+}
+
+// 易懂的题解
+func threeSum2(nums []int) [][]int {
+	n := len(nums)
+	res := make([][]int, 0)
+
+	if n < 3 {
+		return res
+	}
+	sort.Ints(nums)
+
+	for i := 0; i < n; i++ {
+		if nums[i] > 0 {
+			return res
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		L := i + 1
+		R := n - 1
+		for L < R {
+			if nums[i]+nums[L]+nums[R] == 0 {
+				res = append(res, []int{nums[i], nums[L], nums[R]})
+				for L < R && nums[L] == nums[L+1] {
+					L++
+				}
+				for L < R && nums[R] == nums[R-1] {
+					R--
+				}
+				L++
+				R--
+			} else if nums[i]+nums[L]+nums[R] > 0 {
+				R--
+			} else {
+				L++
+			}
+		}
+	}
+	return res
+}
