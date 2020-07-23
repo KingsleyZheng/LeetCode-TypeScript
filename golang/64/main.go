148b1ab012f403025493a930a4ef96e9d1524eb5
zhengchengzhi <zhengchengzhi@qq.com>
2020-07-23 09:20:26 +0800

    golang 64

---
+++ golang/64/main.go
@@ -0,0 +1,44 @@
+package main
+
+func main() {
+
+}
+
+// 中等
+// 64. 最小路径和
+// 给定一个包含非负整数的 m x n 网格，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+// 说明：每次只能向下或者向右移动一步。
+
+// 看题目就知道是动态规划，但是不会写代码
+// 官方解法：动态规划。
+// 当 i > 0 且 j > 0 时，dp[i][j] = min(dp[i - 1][j], dp[i][j-1]) + grid([i][j])
+func minPathSum(grid [][]int) int {
+	if len(grid) == 0 || len(grid[0]) == 0 {
+		return 0
+	}
+	rows, columns := len(grid), len(grid[0])
+	dp := make([][]int, columns)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, columns)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 1; i < rows; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+	for j := 1; j < columns; j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+	for i := 1; i < rows; i++ {
+		for j := 1; j < columns; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+	return dp[rows-1][columns-1]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
